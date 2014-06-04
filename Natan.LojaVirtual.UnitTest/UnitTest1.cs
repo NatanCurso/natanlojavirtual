f554e4e27889917ff28c28b4500e36ba58466be6
NatanCurso <natan.dutra@aedu.com>
2014-06-04 19:15:41 -0300

    Initial Commit

---
+++ Natan.LojaVirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Natan.LojaVirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestNatan
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
